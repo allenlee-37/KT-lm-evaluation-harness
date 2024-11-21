59d6b5775a44e8e3970c012a75e91693f28a0a8f
allenlee-37 <watersurface37@gmail.com>
2024-11-22 00:40:55 +0900

    UPDATE gpqa, musr exact_match version

--- lm_eval/tasks/leaderboard/musr/utils.py
+++ lm_eval/tasks/leaderboard/musr/utils.py
@@ -1,4 +1,5 @@
 import ast
+import datasets
 
 def doc_to_choice(doc):
     """
@@ -6,7 +7,8 @@ def doc_to_choice(doc):
     """
     return ast.literal_eval(doc["choices"])
 
-DOC_TO_TEXT = "{narrative}\n\n" "{question}\n\n" "{choices}\n" "Answer:"
+# 명확한 질문 전달을 위한 프롬프트 수정
+DOC_TO_TEXT = "Please answer the following question with (a), (b), (c), (d) \n\nNarrative: {narrative}\n\n" "Question: {question}\n\n" "Choices: {choices}\n\n" "Answer: "
 
 def doc_to_text(doc):
     """
@@ -14,10 +16,14 @@ def doc_to_text(doc):
     """
     choices = ""
     for i, choice in enumerate(ast.literal_eval(doc["choices"])):
-        choices += f"{i+1} - {choice}\n"
+        choices += f"({chr(i+97)}) - {choice}\n" # 정답 인덱싱 추가
 
     text = DOC_TO_TEXT.format(
         narrative=doc["narrative"], question=doc["question"], choices=choices
     )
-
     return text
+
+# 정답 보내주는 부분 추가
+def get_answer(doc):
+    # 1~4까지의 인덱스를 a~d로 변환
+    return f"({chr(doc['answer_index']+96)})"
--- sh_scripts/on_cpu/meta_llama-3.1-8B-Instruct.sh
+++ sh_scripts/on_cpu/meta_llama-3.1-8B-Instruct.sh
@@ -1,14 +1,14 @@
-cd ./
+cd ../
 model="hf"
-# meta-llama/Llama-3.1-8B-Instruct
-model_args="pretrained=meta-llama/Llama-3.1-8B-Instruct" 
-tasks="bbh_cot_fewshot_boolean_expressions"
+model_args="pretrained=meta-llama/Llama-3.1-8B-Instruct"
+tasks="leaderboard_gpqa"
+
 lm_eval --model $model \
     --model_args $model_args \
     --tasks $tasks \
     --output_path output \
-    --batch_size auto \
+    --batch_size 1 \
     --log_samples \
     --apply_chat_template \
     --limit 10 \
-    --device cpu # 이건 mac 용도: 서버의 경우 삭제 필요
+    --device cpu
