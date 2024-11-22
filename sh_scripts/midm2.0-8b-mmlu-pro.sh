e49910b0f36d77b4a7bdd414ed45ed7f3885a5cf
allenlee-37 <watersurface37@gmail.com>
2024-11-22 15:41:14 +0900

    update on scripts

---
+++ sh_scripts/midm2.0-8b-mmlu-pro.sh
@@ -0,0 +1,12 @@
+cd ../
+model="hf"
+model_args="pretrained=/vfroot/n22-largeai/studio/Midm-bitext-S-Chat/midm-bitext-s-chat-200"
+tasks="leaderboard_mmlu_pro"
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6 
+lm_eval --model $model \
+    --model_args $model_args \
+    --tasks $tasks \
+    --output_path output \
+    --batch_size auto \
+    --log_samples \
+    --apply_chat_template
