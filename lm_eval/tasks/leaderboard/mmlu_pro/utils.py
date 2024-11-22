809e71c1012c4c0312570cb67ed8bb145baefcb1
allenlee-37 <watersurface37@gmail.com>
2024-11-22 13:26:39 +0900

    UPDATE MMLU-pro ver0.1

--- lm_eval/tasks/leaderboard/mmlu_pro/utils.py
+++ lm_eval/tasks/leaderboard/mmlu_pro/utils.py
@@ -5,11 +5,13 @@ def doc_to_text(doc):
     doc_to_text = f"{doc['question']}\n"
 
     for i in range(len(doc["options"])):
-        doc_to_text += f"{string.ascii_uppercase[i]}. {doc['options'][i]}\n"
+        # () 감쌀 수 있게 수정
+        doc_to_text += f"({string.ascii_uppercase[i]}). {doc['options'][i]}\n"
 
     doc_to_text += "Answer:"
     return doc_to_text
 
 
 def doc_to_choice(doc):
-    return [string.ascii_uppercase[i] for i in range(len(doc["options"]))]
+    # () 감쌀 수 있게 수정
+    return [f"({string.ascii_uppercase[i]})" for i in range(len(doc["options"]))]
