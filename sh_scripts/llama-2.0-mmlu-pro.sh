aa719f760eb55fb4093a2c05c402a09ed6967ec1
allenlee-37 <watersurface37@gmail.com>
2024-11-25 14:41:44 +0900

    ready for 2.0 test modelingteam version

---
+++ sh_scripts/llama-2.0-mmlu-pro.sh
@@ -0,0 +1,13 @@
+cd ../
+model="hf"
+model_args="pretrained=/vfroot/n22-model/f6e12a6705dc47f5af9f2b6bce90e0d4/93/40/e3564e474cf19dfb7e2fc364fe61/llama3.1-pro16-0827-Inst-nc-chat_241016"
+tasks="leaderboard_mmlu_pro"
+
+lm_eval --model $model \
+    --model_args $model_args \
+    --tasks $tasks \
+    --output_path output \
+    --batch_size auto \
+    --log_samples \
+    --apply_chat_template \
+    --device cpu
