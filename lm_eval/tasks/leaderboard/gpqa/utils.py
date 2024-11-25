b6ba89a574e62619e916ca2ce5605653e3090dbf
allenlee-37 <watersurface37@gmail.com>
2024-11-25 14:38:03 +0900

    modelingn team version code inserted

--- lm_eval/tasks/leaderboard/gpqa/utils.py
+++ lm_eval/tasks/leaderboard/gpqa/utils.py
@@ -4,7 +4,7 @@ import re
 import datasets
 
 
-def preprocess(text):
+def preprocess(text): # 텍스트 정리
     if text is None:
         return " "
     text = text.strip()
@@ -17,22 +17,23 @@ def preprocess(text):
 def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
     def _process_doc(doc):
         choices = [
-            preprocess(doc["Incorrect Answer 1"]),
+            preprocess(doc["Incorrect Answer 1"]), # 텍스트 정리가 된 보기들
             preprocess(doc["Incorrect Answer 2"]),
             preprocess(doc["Incorrect Answer 3"]),
             preprocess(doc["Correct Answer"]),
         ]
 
-        random.shuffle(choices) # 랜덤 셔플한 초이스
-        correct_answer_index = choices.index(preprocess(doc["Correct Answer"])) # 정답 인덱스
+        random.shuffle(choices) # 보기들을 섞음
+        correct_answer_index = choices.index(preprocess(doc["Correct Answer"])) # 정답의 인덱스 찾기
 
-        out_doc = {
+        out_doc = { # 최종 데이터셋 딕셔너리 형태로 반환
             "choice1": choices[0],
             "choice2": choices[1],
             "choice3": choices[2],
             "choice4": choices[3],
             "answer": f"({chr(65 + correct_answer_index)})",
+            "answer_str": f"{chr(65 + correct_answer_index)}", # 정답은 문자열로 나옴 (A, B, C, D)
         }
         return out_doc
 
-    return dataset.map(_process_doc)
+    return dataset.map(_process_doc) # 데이터셋에 매핑
