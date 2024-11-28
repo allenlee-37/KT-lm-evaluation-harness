cb86a427f4dc1c0c48085a0f8f628f4ce1b0efaf
allenlee-37 <watersurface37@gmail.com>
2024-11-28 13:37:34 +0900

    UPDATE KoBEST

---
+++ sh_scripts/gpt4o-kobest.sh
@@ -0,0 +1,12 @@
+model="openai-chat-completions"
+model_args="model=gpt-4"
+tasks="kobest" # leaderboard_bbh_boolean_expressions
+
+lm_eval --model $model \
+    --model_args $model_args \
+    --tasks $tasks \
+    --output_path output \
+    --batch_size auto \
+    --log_samples \
+    --apply_chat_template \
+    --limit 10
